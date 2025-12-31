19c572e7fd98388c32d35e73588ab0d374938b9b
Joseph Tabora <joetabora@gmail.com>
2025-12-30 20:02:32 -0600

    Fix remaining wrench-DEFAULT reference in login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -246,7 +246,7 @@ export default function LoginPage() {
               <div className="w-full border-t border-white/10"></div>
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-wrench-DEFAULT text-gray-400">Or continue with</span>
+              <span className="px-2 bg-wrench text-gray-400">Or continue with</span>
             </div>
           </div>
 
